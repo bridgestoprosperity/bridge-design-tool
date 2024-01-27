311bd42b03e359e4715e3eb2fafb5adb93cbc4a3
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2024-01-26 17:05:39 -0800

    Publishing basic app

---
+++ Bridge_Design_Tool.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Bridge Design Tool",
+    page_icon="🌉",
+)
+
+st.sidebar.subheader("Select a bridge type above.")
+
+st.markdown(
+    """
+## Choose your bridge type in the sidebar on the left
+
+More instructions would be on this page.
+"""
+)
---
+++ pages/2_Other_Bridge_Type.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+
+st.set_page_config(page_title="Other Design", page_icon="🌍")
+
+st.markdown("# Other Bridge Type Design Tool")
+
